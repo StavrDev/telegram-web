47d4d205f0ad9a04ab5e6cb78ac2b4209a27d4f1
StavrDev <140498451+StavrDev@users.noreply.github.com>
2023-09-07 17:57:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
 import config as c
-from aiogram import Dispatcher, Bot, executor, types
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.types.web_app_info import WebAppInfo
+from utils import TgBot
 
+bot = TgBot(c.TOKEN_BOT)
 
-bot = Bot(c.TOKEN_BOT)
-dp = Dispatcher(bot)
-
-async def Start(message: types.Message):
-    await message.answer('Привет!\nДля введи то что хочешь найти в Википедия')
-
-async def Search(message:types.Message):
-    message_text = message.text
-    btn1 = InlineKeyboardButton("Веб-приложение", web_app=WebAppInfo(url = f'https://ru.wikipedia.org/wiki/{message_text}'))
-    keyboard = InlineKeyboardMarkup().add(btn1)
-    await message.answer("Переходи по ссылке и читай!", reply_markup=keyboard)
-
-dp.register_message_handler(Start, commands=['start'])
-dp.register_message_handler(Search)
-
-if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+if __name__ == '__main__':
+    bot.start()
